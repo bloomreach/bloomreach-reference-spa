f396d9adbef64171dc7e8e220fd45b0e085aaee2
Michael Dokolin <mikhail.dokolin@bloomreach.com>
2020-09-02 18:03:43 +0200

    CMS-13874 Add routing support

--- src/index.tsx
+++ src/index.tsx
@@ -16,12 +16,24 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/(.*)" component={App} />
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
