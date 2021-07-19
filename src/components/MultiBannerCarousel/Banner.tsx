aaa046e222cbf66b05d1008cb0b47bc15bc8baf8
Giacomo Lamonaco <giacomo.lamonaco@bloomreach.com>
2021-07-19 07:03:24 +0000

    ENT-3154: Converting Banner Gallery component to X-Component

--- src/components/MultiBannerCarousel/Banner.tsx
+++ src/components/MultiBannerCarousel/Banner.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2020-2021 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,23 +15,22 @@
  */
 
 import React from 'react';
-import { Image } from 'react-bootstrap';
+import { Button, Carousel, Image } from 'react-bootstrap';
 import { Document, ImageSet } from '@bloomreach/spa-sdk';
-import { BrManageContentButton, BrPageContext } from '@bloomreach/react-sdk';
+import { BrPageContext } from '@bloomreach/react-sdk';
 
 import { Link } from '../Link';
 import styles from './Banner.module.scss';
 
 interface BannerProps extends React.ComponentPropsWithoutRef<'a'> {
-  document: Document;
-  parameterName?: string;
+  document: BannerDocument;
 }
 
 export const Banner = React.forwardRef(
-  ({ document, parameterName, className, ...props }: BannerProps, ref: React.Ref<HTMLAnchorElement>) => {
+  ({ document, className, ...props }: BannerProps, ref: React.Ref<HTMLAnchorElement>) => {
     const page = React.useContext(BrPageContext);
 
-    const { image: imageRef, link: linkRef, title } = document.getData<BannerDocument>();
+    const { image: imageRef, link: linkRef, title, content, cta } = document;
     const link = linkRef && page?.getContent<Document>(linkRef);
     const image = imageRef && page?.getContent<ImageSet>(imageRef)?.getOriginal();
 
@@ -42,16 +41,28 @@ export const Banner = React.forwardRef(
         className={`${styles.banner} ${page?.isPreview() ? 'has-edit-button' : ''} ${className ?? ''}`}
         {...props}
       >
-        <BrManageContentButton
-          content={document}
-          documentTemplateQuery="new-banner-document"
-          folderTemplateQuery="new-banner-folder"
-          parameter={parameterName ?? 'document'}
-          root="brxsaas/banners"
-          relative
-        />
-
         {image && <Image className={`${styles.banner__image} d-block w-100 h-100`} src={image.getUrl()} alt={title} />}
+
+        <Carousel.Caption>
+          {title && (
+            <h3 className="my-0">
+              <mark className="d-inline-block">{title}</mark>
+            </h3>
+          )}
+          {content?.value && (
+            <div
+              className={`${styles.banner__contents} d-inline-block mark mt-1`}
+              dangerouslySetInnerHTML={{ __html: page?.rewriteLinks(content.value) ?? '' }}
+            />
+          )}
+          {cta && (
+            <div className="mt-2">
+              <Button as={Link} variant="primary" href={link?.getUrl()}>
+                {cta}
+              </Button>
+            </div>
+          )}
+        </Carousel.Caption>
       </Link>
     );
   },
--- src/components/MultiBannerCarousel/MultiBannerCarousel.tsx
+++ src/components/MultiBannerCarousel/MultiBannerCarousel.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2020-2021 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,56 +18,30 @@ import React from 'react';
 import { Carousel, Col, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { Reference } from '@bloomreach/spa-sdk';
-import { BrManageContentButton, BrProps } from '@bloomreach/react-sdk';
-import { getEffectiveMultipleDocumentParameters } from '../param-utils';
+import { ContainerItem, getContainerItemContent } from '@bloomreach/spa-sdk';
+import { BrProps } from '@bloomreach/react-sdk';
 
 import { Banner } from './Banner';
 import styles from './MultiBannerCarousel.module.scss';
 
-const MAX_DOCUMENTS = 10;
 const DOCUMENTS_PER_SLIDE = 4;
-
-interface MultiBannerCarouselModels {
-  document1?: Reference;
-  document2?: Reference;
-  document3?: Reference;
-  document4?: Reference;
-  document5?: Reference;
-  document6?: Reference;
-  document7?: Reference;
-  document8?: Reference;
-  document9?: Reference;
-  document10?: Reference;
-}
 
 interface MultiBannerCarouselParameters {
   interval?: number;
-  title?: string;
 }
 
-export function MultiBannerCarousel({ component, page }: BrProps): React.ReactElement | null {
-  const { interval = 0, title } = component.getParameters<MultiBannerCarouselParameters>();
-  const models = component.getModels<MultiBannerCarouselModels>();
-  const docParams = getEffectiveMultipleDocumentParameters(page, models, MAX_DOCUMENTS);
+interface MultiBannerCarouselCompound {
+  title: string;
+  banners: BannerDocument[];
+}
+
+export function MultiBannerCarousel({ component, page }: BrProps<ContainerItem>): React.ReactElement | null {
+  const { interval = 0 } = component.getParameters<MultiBannerCarouselParameters>();
+  const { title, banners } = getContainerItemContent<MultiBannerCarouselCompound>(component, page) ?? {};
   const slides = [];
 
-  while (docParams.length) {
-    slides.push(docParams.splice(0, DOCUMENTS_PER_SLIDE));
-  }
-
-  if (!slides.length) {
-    return page.isPreview() ? (
-      <div className="has-edit-button">
-        <BrManageContentButton
-          documentTemplateQuery="new-banner-document"
-          folderTemplateQuery="new-banner-folder"
-          parameter="document1"
-          root="brxsaas/banners"
-          relative
-        />
-      </div>
-    ) : null;
+  while (banners?.length) {
+    slides.push(banners.splice(0, DOCUMENTS_PER_SLIDE));
   }
 
   return (
@@ -85,14 +59,9 @@ export function MultiBannerCarousel({ component, page }: BrProps): React.ReactEl
           // eslint-disable-next-line react/no-array-index-key
           <Carousel.Item key={index}>
             <Row>
-              {slide.map((docParam) => (
-                <Col
-                  key={docParam.document.getId()}
-                  as={Banner}
-                  xs={12 / DOCUMENTS_PER_SLIDE}
-                  document={docParam.document}
-                  parameterName={docParam.parameterName}
-                />
+              {slide.map((banner, internalIndex) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <Col key={`${index}-${internalIndex}`} as={Banner} xs={12 / DOCUMENTS_PER_SLIDE} document={banner} />
               ))}
             </Row>
           </Carousel.Item>
